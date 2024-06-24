56d88fab0b817e46b6bf925391b4d649602efb1a
Tristan Montoya <montoya.tristan@gmail.com>
2024-06-25 00:21:51 +0200

    remove GFlops

--- src/ReproduceEntropyStableDSEM.jl
+++ src/ReproduceEntropyStableDSEM.jl
@@ -2,28 +2,13 @@ module ReproduceEntropyStableDSEM
 
     using JLD2
     using OrdinaryDiffEq
-    using LinearAlgebra
-    using LinearMaps: UniformScalingMap
-    using GFlops
     using TimerOutputs
     using LaTeXStrings
-    using UnPack
-    using Arpack
-    using KrylovKit: eigsolve
-    using Dates
     using Suppressor
-    using Plots
-    using IterativeSolvers: powm
     using StableSpectralElements
 
     export run_driver
     
-    export AdvectionDriver
-    include("advection_refinement.jl")
-
-    export AdvectionPRefinementDriver
-    include("advection_p_refinement.jl")
-
     export EulerDriver
     include("euler_refinement.jl")
 
